9a7ea699b5aa7b8c99d84034710bcaa1f87df49b
Alexander Naumov <me@alexandernaumov.de>
2019-10-29 10:59:03 +0100

    improve address builder

--- src/Helpers/AddressBuilder.php
+++ src/Helpers/AddressBuilder.php
@@ -6,47 +6,103 @@ namespace Alnv\ContaoGeoCodingBundle\Helpers;
 class AddressBuilder {
 
 
-    protected $strAddress = '';
+    protected $arrAddressFields = ['street','streetNumber','zip','city','state','country'];
+    protected $arrAddress = ['address' => [], 'city' => [], 'state' => [], 'country' => []];
 
 
-    public function setZip( $strZip ) {
+    public function __construct( $arrAddress ) {
 
-        $this->strAddress .= $strZip ? ' ' . $strZip : '';
+        foreach ( $this->arrAddressFields as $strField ) {
+
+            if ( isset( $arrAddress[ $strField ] ) && $arrAddress[ $strField ] !== '' ) {
+
+                $strMethod = 'set' . ucfirst( $strField );
+                $this->{$strMethod}( $arrAddress[ $strField ] );
+            }
+        }
     }
 
 
-    public function setCity( $strCity ) {
+    protected function setZip( $strZip ) {
 
-        $this->strAddress .= $strCity ? ' ' . $strCity . ',' : ',';
+        if ( $strZip === '' || $strZip === null ) {
+
+            return null;
+        }
+
+        $this->arrAddress['city'][] = $strZip;
     }
 
 
-    public function setStreet( $strStreet ) {
+    protected function setCity( $strCity ) {
 
-        $this->strAddress .= $strStreet ?: '';
+        if ( $strCity === '' || $strCity === null ) {
+
+            return null;
+        }
+
+        $this->arrAddress['city'][] = $strCity;
     }
 
 
-    public function setStreetNumber( $strStreetNumber ) {
+    protected function setStreet( $strStreet ) {
 
-        $this->strAddress .= $strStreetNumber ? ' ' . $strStreetNumber . ',' : ',';
+        if ( $strStreet === '' || $strStreet === null ) {
+
+            return null;
+        }
+
+        $this->arrAddress['address'][] = $strStreet;
     }
 
 
-    public function setCountry( $strCountry ) {
+    protected function setStreetNumber( $strStreetNumber ) {
 
-        $this->strAddress .= $strCountry ? ' ' . $strCountry : '';
+        if ( $strStreetNumber === '' || $strStreetNumber === null ) {
+
+            return null;
+        }
+
+        $this->arrAddress['address'][] = $strStreetNumber;
     }
 
 
-    public function setState( $strState ) {
+    protected function setCountry( $strCountry ) {
 
-        $this->strAddress .= $strState ? ' ' . $strState : '';
+        if ( $strCountry === '' || $strCountry === null ) {
+
+            return null;
+        }
+
+        $this->arrAddress['country'][] = $strCountry;
+    }
+
+
+    protected function setState( $strState ) {
+
+        if ( $strState === '' || $strState === null ) {
+
+            return null;
+        }
+
+        $this->arrAddress['state'][] = $strState;
     }
 
 
     public function getAddress() {
 
-        return trim( $this->strAddress );
+        $arrAddress = [];
+
+        foreach ( $this->arrAddress as $arrBlock ) {
+
+            if ( empty( $arrBlock ) ) {
+
+                continue;
+            }
+
+            $arrAddress[] = implode( ', ', $arrBlock );
+        }
+
+        return implode( ', ', $arrAddress );
     }
 }
--- src/Helpers/Toolkit.php
+++ src/Helpers/Toolkit.php
@@ -8,37 +8,14 @@ class Toolkit {
 
     public static function generateAddress( $arrGeoSettings, $arrRecord ) {
 
-        $objAddress = new AddressBuilder();
+        $arrAddress = [];
 
-        if ( $arrGeoSettings['street'] ) {
+        foreach ( $arrGeoSettings as $strGeoField ) {
 
-            $objAddress->setStreet( $arrRecord[ $arrGeoSettings['street'] ] );
+            $arrAddress[ $strGeoField ] = $arrRecord[ $arrGeoSettings['street'] ];
         }
 
-        if ( $arrGeoSettings['streetNumber'] ) {
-
-            $objAddress->setStreetNumber( $arrRecord[ $arrGeoSettings['streetNumber'] ] );
-        }
-
-        if ( $arrGeoSettings['zip'] ) {
-
-            $objAddress->setZip( $arrRecord[ $arrGeoSettings['zip'] ] );
-        }
-
-        if ( $arrGeoSettings['city'] ) {
-
-            $objAddress->setCity( $arrRecord[ $arrGeoSettings['city'] ] );
-        }
-
-        if ( $arrGeoSettings['state'] ) {
-
-            $objAddress->setState( $arrRecord[ $arrGeoSettings['state'] ] );
-        }
-
-        if ( $arrGeoSettings['country'] ) {
-
-            $objAddress->setCountry( $arrRecord[ $arrGeoSettings['country'] ] );
-        }
+        $objAddress = new AddressBuilder( $arrAddress );
 
         return $objAddress->getAddress();
     }
