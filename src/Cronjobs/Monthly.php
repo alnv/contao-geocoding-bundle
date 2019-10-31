1ece04fcb13db5ace364d7a7c9e8a21409ab3eb4
Alexander Naumov <me@alexandernaumov.de>
2019-10-31 09:16:58 +0100

    impl cronjob

---
+++ src/Cronjobs/Monthly.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Alnv\ContaoGeoCodingBundle\Cronjobs;
+
+use Alnv\ContaoGeoCodingBundle\Helpers\Automation;
+
+
+class Monthly {
+
+
+    public function clearGeoCodingCache() {
+
+        Automation::clearGeoCodingCacheByInterval( 14 );
+    }
+}
