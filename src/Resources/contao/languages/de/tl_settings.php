414567bde8a7dc52f012a6f8e223bd371940b051
Alexander Naumov <mail@alexandernaumov.de>
2021-11-13 09:25:15 +0100

    change label

--- src/Resources/contao/languages/de/tl_settings.php
+++ src/Resources/contao/languages/de/tl_settings.php
@@ -1,4 +1,4 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_settings']['geocoding_settings'] = 'GeoCoding-Einstellungen';
-$GLOBALS['TL_LANG']['tl_settings']['googleMapsApiKey'] = [ 'Google API Schlüssel', '' ];
+$GLOBALS['TL_LANG']['tl_settings']['geocoding_settings'] = 'Karten-Einstellungen';
+$GLOBALS['TL_LANG']['tl_settings']['googleMapsApiKey'] = ['Google-API Schlüssel', 'Hier können Sie den Lizenzschlüssel für Geo-Coding und JS-Maps eingeben.'];
